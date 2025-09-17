c684691998be8e757dfab6700a3372b51c2e91b2
Ishaq Ahmed <iahmed70@users.noreply.github.com>
2025-09-17 21:45:06 +0000

    updates to villain-path

--- story.py
+++ story.py
@@ -13,6 +13,7 @@ def left_path():
 
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
+    print("you fought the squirrel bravely. However, at the end, you lost")
 
 if __name__ == "__main__":
     intro()
